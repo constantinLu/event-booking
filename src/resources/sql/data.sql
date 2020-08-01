2bf6287c5dd3cc6a56852ce4f0727a3df5ec50c9
florin.tansanu <florin.tansanu@gmail.com>
2020-08-01 16:17:42 +0300

    EB-6 Added data.sql

---
+++ src/resources/sql/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES ('1', 'Alex', 'Cumparatu', 'alex.cumparatu@m.com', 'alex.cumparatu', 'alex', 'STUDENT');
+
+
+INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES ('2', 'Stuart', 'Max', 'stuart.max@m.com', 'stuart.max', 'stuart', 'STUDENT');
+
+INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES ('3', 'Ben', 'Wheel', 'ben.wheel@m.com', 'ben.wheel', 'ben', 'EVENT_ORGANISER');
+
+INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES ('4', 'Mason', 'Jackson', 'mason.jackson@m.com', 'mason.jackson', 'mason', 'ADMINISTRATOR');
