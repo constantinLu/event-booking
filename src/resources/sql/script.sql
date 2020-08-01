3bef8d94046a98672d0a15fb2c3f46bbe1110be7
florin.tansanu <florin.tansanu@gmail.com>
2020-08-01 14:37:40 +0300

    EB-3 Added script sql and adapted the user class

---
+++ src/resources/sql/script.sql
@@ -0,0 +1,41 @@
+create database event_booking;
+
+
+create table user(
+id BIGINT PRIMARY KEY auto_increment,
+first_name varchar(100),
+last_name varchar(100),
+email varchar(500),
+username varchar(100),
+password varchar(100),
+role varchar (100)
+);
+
+create table user_event(
+id bigint primary key auto_increment,
+user_id BIGINT,
+event_id BIGINT
+);
+
+create table event(
+id bigint primary key auto_increment,
+title varchar(100),
+description varchar(2000),
+is_online char,
+location varchar(100),
+start_date datetime,
+end_date datetime,
+limitations varchar(2000),
+is_booking_allowed char
+);
+
+create table booking(
+id bigint primary key auto_increment,
+booking_date datetime,
+cancel_date datetime,
+user_id bigint,
+event_id bigint
+);
+
+
+
--- src/utils/Test/Test.java
+++ src/utils/Test/Test.java
@@ -1,21 +1,29 @@
-//package utils.Test;
-//
-//
-//import entities.Entity;
-//import java.util.ArrayList;
-//import networking.JDBC;
-//
-//public class Test {
-//
-//    public static void printTable (String table, String className) {
-//        System.out.println("-----------------------------------");
-//        System.out.println(String.format("ALL %s", table));
-//        String query = String.format("SELECT * FROM %s", table);
-//       // ArrayList<Entity> obj = JDBC.getAll(query, className);
-//        for (Entity object : obj) {
-//            System.out.println(object.toString());
-//            System.out.println("***");
-//        }
-//        System.out.println("------------------------------------");
-//    }
-//}
+package Utils.Test;
+
+
+import entities.Entity;
+import java.util.ArrayList;
+
+import entities.User;
+import networking.DBTables;
+import networking.JDBC;
+
+public class Test {
+    public static void main(String[] args) {
+        printTable(DBTables.USER_TABLE, User.class.getName());
+    }
+
+    public static void printTable (String table, String className) {
+
+        JDBC.createConnection();
+        System.out.println("-----------------------------------");
+        System.out.println(String.format("ALL %s", table));
+        String query = String.format("SELECT * FROM %s", table);
+        ArrayList<Entity> obj = JDBC.getAll(query, className);
+        for (Entity object : obj) {
+            System.out.println(object.toString());
+            System.out.println("***");
+        }
+        System.out.println("------------------------------------");
+    }
+}
