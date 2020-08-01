57361fe07eaf92d9bb6d478284eafc0c211dfd1e
Lungu Constantin <gizet_st@outlook.com>
2020-08-01 11:19:26 +0300

    EB-2- ExampleData -fix #2

---
+++ src/Utils/Test/Test.java
@@ -0,0 +1,21 @@
+//package Utils.Test;
+//
+//
+//import entities.Entity;
+//import java.util.ArrayList;
+//import networking.JDBC;
+//
+//public class Test {
+//
+//    public static void printTable (String table, String className) {
+//        System.out.println("-----------------------------------");
+//        System.out.println(String.format("ALL %s", table));
+//        String query = String.format("SELECT * FROM %s", table);
+//       // ArrayList<Entity> obj = JDBC.getAll(query, className);
+//        for (Entity object : obj) {
+//            System.out.println(object.toString());
+//            System.out.println("***");
+//        }
+//        System.out.println("------------------------------------");
+//    }
+//}
---
+++ src/Utils/Validators.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import View.NotificationPane;
+import javafx.scene.control.TextField;
+
+public class Validators {
+
+    private Validators() {
+    }
+
+    @SafeVarargs
+    public static <T extends TextField> boolean validate(T... widget) {
+        if (widget.length != 0) {
+            for (T element : widget) {
+                if (element.getText().isEmpty()) {
+                    NotificationPane.show("Some fields are empty");
+                    return false;
+                } else if (element.getText().length() < 4) {
+                    NotificationPane.show("Entry is too short");
+                    return false;
+                }
+            }
+            return true;
+        } else return false;
+    }
+
+
+}
