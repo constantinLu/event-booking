fcfb640e912523a3b9716f4ba138fe53738dabac
Lungu Constantin <gizet_st@outlook.com>
2020-08-03 10:34:22 +0300

    EB-15 - Sql Data

--- src/resources/sql/data.sql
+++ src/resources/sql/data.sql
@@ -6,3 +6,10 @@ INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `u
 INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES ('3', 'Ben', 'Wheel', 'ben.wheel@m.com', 'ben.wheel', 'ben', 'EVENT_ORGANISER');
 
 INSERT INTO `event_booking`.`user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES ('4', 'Mason', 'Jackson', 'mason.jackson@m.com', 'mason.jackson', 'mason', 'ADMINISTRATOR');
+
+
+
+INSERT INTO `event_booking`.`event` (`title`, `description`, `is_online`, `location`, `start_date`, `end_date`, `constraints`, `is_booking_allowed`, `organiser_id`) VALUES
+('Jack the Reaper', 'Movie in the town', 'n', 'Liverpool', '2020-03-20 23:59:59', '2020-04-02 23:59:59', '100', 'y', '2'),
+('Ministry of Sound', 'Movie in the town', 'n', 'London', '2020-05-20 23:59:59', '2020-05-22 23:59:59', '100', 'y', '1');
+
